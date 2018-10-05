f9ae9cbe03c64d79ed1de8f46b75d1c9b4f2afae
suprematis <suprematis@malevich-big-data.lan>
2018-10-05 06:43:23 -0700

    Script running with set CSV file

--- sms_workflow.py
+++ sms_workflow.py
@@ -7,15 +7,14 @@ with open('numbers.csv') as csv_numbers:
 	for row in csv_reader:
 #		print('{}'.format(row[0]))
 		array_num.append(row[0])
-
+body_input=input('Type the message body:')
 account_sid = 'AC73b99b46f6fabe9ab60147ed45b58215' 
 auth_token = 'cee21593cdd05684ed6027bfd2b94a24' 
 client = Client(account_sid, auth_token) 
 for number in array_num: 
 	message = client.messages.create( 
-                             	from_='+15107563068',  
-				body='This is a test message',      
+                             	from_='15107565042',  
+				body=body_input,      
                              	to=number
-                          ) 
- 
+                          )  
 print(message.sid)
