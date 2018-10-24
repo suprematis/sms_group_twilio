830ab402c14ba557e4d39b2b37328593ae398843
Andres Salgado <as@andresalgado.com>
2018-10-24 10:06:23 -0700

    Removed API keys from script

--- python_twilio_sms.py
+++ python_twilio_sms.py
@@ -7,8 +7,6 @@ def twilio_api_sms():
                 global array_numbers
                 body_input = input('Type the message body:')
 #                twilioconfig = read_twilio_config()
-                account_sid = 'AC73b99b46f6fabe9ab60147ed45b58215'
-                auth_token = 'cee21593cdd05684ed6027bfd2b94a24'
                 client = Client(account_sid, auth_token)
                 for number in array_numbers:
                         message = client.messages.create(
